616ed4e3678d991a02c04b6f8792a7d179d2d968
JUANCITOPENA <JUANCITO.PENA.V@HOTMAIL.COM>
2020-10-08 22:57:41 -0400

    first commit

--- appfirebase.js
+++ appfirebase.js
@@ -51,14 +51,14 @@ auth.onAuthStateChanged(function(user) {
 
         var email = user.email;
         alert("Inicio de Seccion Correcto! " + email);
-        //window.open("https://app.powerbi.com/view?r=eyJrIjoiNGRiOTA3NDMtM2ViNC00MDQ2LThjOGQtMzA1MzJhY2Q2ZjAwIiwidCI6IjZkZjY3NDI4LTMwYzYtNDk3Ny1iMTIxLWY1OWZlM2FjZjdhMCIsImMiOjJ9")
+        window.open("https://app.powerbi.com/view?r=eyJrIjoiNGRiOTA3NDMtM2ViNC00MDQ2LThjOGQtMzA1MzJhY2Q2ZjAwIiwidCI6IjZkZjY3NDI4LTMwYzYtNDk3Ny1iMTIxLWY1OWZlM2FjZjdhMCIsImMiOjJ9")
 
         //window.close("index.html")
 
-        window.open("reporte.html")
+        //window.open("reporte.html")
         auth.signIn();
-        window.close("index.html")
-            //Take user to a different or home page`
+        // window.close("index.html")
+        //Take user to a different or home page`
 
         //is signed in
 
